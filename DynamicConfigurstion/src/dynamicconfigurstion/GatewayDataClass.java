c22cf25456657fc90618fffdaa69c8737a1919ff
Faize1997 <muhammadfaizanzaffar@hotmail.com>
2020-05-18 01:16:47 +0500

    Model Classes Are Included to populate data

---
+++ DynamicConfigurstion/src/dynamicconfigurstion/GatewayDataClass.java
@@ -0,0 +1,27 @@
+
+package dynamicconfigurstion;
+
+import java.util.List;
+
+public class GatewayDataClass {
+    private String gatewayName;
+    private List<String> gatewayValues;
+
+    public String getGatewayName() {
+        return gatewayName;
+    }
+
+    public void setGatewayName(String gatewayName) {
+        this.gatewayName = gatewayName;
+    }
+
+    public List<String> getGatewayValues() {
+        return gatewayValues;
+    }
+
+    public void setGatewayValues(List<String> gatewayValues) {
+        this.gatewayValues = gatewayValues;
+    }
+    
+    
+}
